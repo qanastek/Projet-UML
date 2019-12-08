89c146fa1f017a6f30cc52d4134a117b9815716b
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2019-12-08 23:15:29 +0100

    - Fix the FXML

--- src/fr/univavignon/ceri/application/Main.java
+++ src/fr/univavignon/ceri/application/Main.java
@@ -3,7 +3,7 @@ package src.fr.univavignon.ceri.application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
@@ -14,7 +14,7 @@ public class Main extends Application {
 			// Set the title of the window
 			primaryStage.setTitle("Projet UML - V1.1");
 			
-			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Gui.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Gui.fxml"));
 			
 			Scene scene = new Scene(root,800,800);
 			
